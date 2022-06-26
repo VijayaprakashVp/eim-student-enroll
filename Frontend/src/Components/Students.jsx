b4bd0a2d6dbb1e3dd82d1dc556a8369b0e752a0e
Prakash <VijayaprakashVp>
2022-06-26 11:55:47 +0530

    frontend struture and required packages installed

---
+++ Frontend/src/Components/Students.jsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
+
+const Students = () => {
+  return (
+    <div>
+      <Heading>EIM-Students-enroll</Heading>
+    </div>
+  );
+};
+
+export default Students;
